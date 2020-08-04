ca6a14fe5f78f3f6165dbe8e8dec25a8856d0d98
Michael Racz <39655509+Michael-Racz@users.noreply.github.com>
2020-08-03 23:17:54 -0500

    Add files via upload

---
+++ readCensusExcel.py
@@ -0,0 +1,43 @@
+'''
+This is what your program does:
+
+    Reads the data from the Excel spreadsheet
+    Counts the number of census tracts in each county
+    Counts the total population of each county
+    Prints the results
+
+'''
+
+
+
+import openpyxl, pprint
+import os
+
+print('Opening workbook...')
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+sheet = wb['Population by Census Tract']
+countyData = {}
+
+#TODO: Fill in countyData wtih each county's population and tracts
+print('Reading rows...')
+for row in range(2, sheet.max_row + 1):
+    #Each row in the sheet has data for one census tract
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+    #Make sure the key for the state exists
+    countyData.setdefault(state, {})
+    #Make sure the key for this county in this state exists
+    countyData[state].setdefault(county,{'tracts':0,'pop':0})
+    #each row represents one census tract, so increment by one.
+    countyData[state][county]['tracts'] +=1
+    #increase the county pop by the pop in this census tract
+    countyData[state][county]['pop'] += int(pop)
+
+
+#TODO: Open a new text file and write the contents of countyData to it.
+print('Writing results..')
+resultFile = open('census2010.py','w')
+resultFile.write('allData = '+ pprint.pformat(countyData))
+resultFile.close()
+print('Done')
